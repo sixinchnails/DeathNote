e5e973b78dddcf807482802efc872f0f2927d383
김라현 <wwldwwld7@naver.com>
2023-10-31 09:29:16 +0900

    Feat : 노트 사라지는 애니메이션

--- DeathNote/Assets/Script/Common/TalkManager.cs
+++ DeathNote/Assets/Script/Common/TalkManager.cs
@@ -28,13 +28,16 @@ public class TalkManager : MonoBehaviour
         scriptList.Add(0, openingData);
 
         List<TalkData> opening2Data = new List<TalkData> {
-            new TalkData(2, "『너 어떻게 정령을 불러낸 거지?』"),
-            new TalkData(2, "『이 악보를 완벽히 연주해야만 정령이 풀려나는데』"),
-            new TalkData(1, "『어어..! 넌 뭐야』"),
-            new TalkData(2, "『이 악보엔 정령들이 봉인되어 있어.』"),
-            new TalkData(2, "『하지만, 악보가 찢어져서 뿔뿔이 흩어지고 말았지.』"),
-            new TalkData(2, "『너라면 정령들을 풀어줄 수 있겠어.』"),
-            new TalkData(2, "『월드 버튼을 눌러봐. 나랑 악보를 찾아 여행을 떠나자.』")
+            new TalkData(2, "『너···! 어떻게 정령을 불러낸 거지?』"),
+            new TalkData(1, "『어어···!  너··· 넌 뭐야!』"),
+            new TalkData(2, "『악보를 완벽히 연주해야만 정령이 풀려나는데···』"),
+            new TalkData(2, "『난 악(樂)신이야』"),
+            new TalkData(2, "『네가 연주한 악보를 보관하고 있었지』"),
+            new TalkData(2, "『악보엔 정령들이 봉인되어 있어』"),
+            new TalkData(2, "『하지만, 악보가 다 찢어져서』"),
+            new TalkData(2, "『정령들이 뿔뿔이 흩어지고 말았어』"),
+            new TalkData(2, "『너라면 악보를 찾아 정령을 풀어줄 수 있을 것 같아』"),
+            new TalkData(2, "『월드 버튼을 눌러봐. 나랑 악보를 찾아 여행을 떠나자』")
         };
         scriptList.Add(-1, opening2Data);
 
@@ -45,7 +48,11 @@ public class TalkManager : MonoBehaviour
 
         List<TalkData> endingData = new List<TalkData>
         {
-            new TalkData(2, "『드디어 정령을 다 구출했구나』")
+            new TalkData(1, "『···  ···!』"),
+            new TalkData(1, "『이 노래··· 어딘가 익숙한데?』"),
+            new TalkData(1, "『내가 어릴 때 작곡한 노래잖아?!』"),
+            new TalkData(2, "『정령을 찾아줘서 고마워 그럼 이만···』"),
+            new TalkData(1, "『안녕···』"),
         };
         scriptList.Add(7, endingData);
     }
