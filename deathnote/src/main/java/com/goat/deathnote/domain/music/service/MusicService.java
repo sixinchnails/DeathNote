7bcc1362fb7b4b5d560f0fe602047ded882fce8f
이경원 <jbs6606@naver.com>
2023-11-15 15:57:52 +0900

    1

--- deathnote/src/main/java/com/goat/deathnote/domain/music/service/MusicService.java
+++ deathnote/src/main/java/com/goat/deathnote/domain/music/service/MusicService.java
@@ -1,10 +1,12 @@
 package com.goat.deathnote.domain.music.service;
 
+import com.goat.deathnote.domain.music.dto.MusicDto;
 import com.goat.deathnote.domain.music.entity.Music;
 import com.goat.deathnote.domain.music.repository.MusicRepository;
-import com.goat.deathnote.domain.world.entity.World;
-import com.goat.deathnote.domain.world.repository.WorldRepository;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,8 +15,11 @@ import java.util.Optional;
 public class MusicService {
 
     private final MusicRepository musicRepository;
+    private final RestTemplate restTemplate;
+    private final String pythonServerUrl = "http://52.79.253.69:5080/compose";
 
-    public MusicService(MusicRepository musicRepository) {
+    public MusicService(MusicRepository musicRepository, RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
         this.musicRepository = musicRepository;
     }
 
@@ -30,4 +35,17 @@ public class MusicService {
         return musicRepository.findById(id);
     }
 
+    public byte[] getAudioFile() {
+        MusicDto requestData = MusicDto.builder().valence(0.5).energy(0.5)   // Example value
+                .acousticness(0.5)  // Example value
+                .danceability(0.5)  // Example value
+                .instrumentalness(0.5)  // Example value
+                .liveness(0.5)  // Example value
+                .loudness(-60)  // Example value
+                .speechiness(0.5)  // Example value
+                .tempo(120).build();// Example value
+
+        ResponseEntity<byte[]> response = restTemplate.postForEntity(pythonServerUrl, requestData, byte[].class);
+        return response.getBody();
+    }
 }
