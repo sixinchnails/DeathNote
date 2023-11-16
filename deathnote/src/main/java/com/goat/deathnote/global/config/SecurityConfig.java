687bfc44be4ae016cb4b234894e4440387a0f0dc
이경원 <jbs6606@naver.com>
2023-11-17 00:39:22 +0900

    로그인실패시 페이지생성

--- deathnote/src/main/java/com/goat/deathnote/global/config/SecurityConfig.java
+++ deathnote/src/main/java/com/goat/deathnote/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.goat.deathnote.global.config;
 
 import com.goat.deathnote.domain.member.entity.MemberRole;
 import com.goat.deathnote.domain.member.service.OAuth2MemberService;
+import com.goat.deathnote.global.oauth.handler.OAuth2AuthenticationFailureHandler;
 import com.goat.deathnote.global.oauth.handler.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +16,13 @@ public class SecurityConfig {
 
     private final OAuth2MemberService oAuth2MemberService;
     private final OAuth2AuthenticationSuccessHandler successHandler;
+    private final OAuth2AuthenticationFailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/게시물등").hasRole(MemberRole.USER.name()) // 특정 ROLE을 가진 사용자만 접근 가능하도록 설정
+                .antMatchers("/**").hasRole(MemberRole.USER.name()) // 특정 ROLE을 가진 사용자만 접근 가능하도록 설정
                 .anyRequest().permitAll() // 모두 접근 가능하도록 설정
                 .and()
 //                .logout()
@@ -30,8 +32,8 @@ public class SecurityConfig {
                 .userInfoEndpoint()
                 .userService(oAuth2MemberService)
                 .and()
-                .successHandler(successHandler);
-
+                .successHandler(successHandler)
+                .failureHandler(failureHandler);
         return http.build();
     }
 
